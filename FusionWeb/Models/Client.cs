7b8cb7b841d82a106617a3a324ab402b2e862a53
levo2001 <levonahaim@gmail.com>
2020-11-26 23:49:55 +0200

    hii

--- FusionWeb/Models/Client.cs
+++ FusionWeb/Models/Client.cs
@@ -7,7 +7,6 @@ namespace FusionWeb.Models
 {
     public class Client
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string  Email { get; set; }
         public string  Address { get; set; }
