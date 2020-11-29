365036300b42f190c77c71287a63c347a670192b
debor <debora.wiesel@gmail.com>
2020-11-29 22:08:17 +0200

    try marge

--- FusionWeb/Models/Credit.cs
+++ FusionWeb/Models/Credit.cs
@@ -7,7 +7,7 @@ namespace FusionWeb.Models
 {
     public class Credit
     {
-        //
+        //2
         public int Id { get; set; }
         public string Name { get; set; }
         public int Number { get; set; }
