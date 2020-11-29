ef2187d99a4e95a915d5e3b6c446281fff5d2a29
debor <debora.wiesel@gmail.com>
2020-11-29 22:05:31 +0200

    change credit

--- FusionWeb/Models/Credit.cs
+++ FusionWeb/Models/Credit.cs
@@ -7,6 +7,7 @@ namespace FusionWeb.Models
 {
     public class Credit
     {
+        //
         public int Id { get; set; }
         public string Name { get; set; }
         public int Number { get; set; }
