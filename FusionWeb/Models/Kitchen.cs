9814da1f05cc48ffe73b0d4b47d50aa12097db64
debor <debora.wiesel@gmail.com>
2020-12-07 22:59:41 +0200

    correct the  Kitchen

--- FusionWeb/Models/Kitchen.cs
+++ FusionWeb/Models/Kitchen.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +8,20 @@ namespace FusionWeb.Models
 {
     public class Kitchen
     {
-        public string Israel { get; set; }
 
-        public string Asian { get; set; }
+        public int Id { get; set; }
 
-        public string American { get; set;}
-        
-        public string Italian { get; set; }
+        [Display(Name = "מטבח ישראלי")]
+        public int IsraelId { get; set; }
+
+        [Display(Name = "מטבח אסיאתי")]
+        public int AsianId { get; set; }
+
+        [Display(Name = "מטבח אמריקאי")]
+        public int AmericanId { get; set;}
+
+        [Display(Name = "מטבח איטלקי")]
+        public int ItalianId { get; set; }
 
 
     }
