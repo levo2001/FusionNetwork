5c8970b480107652204798c27d4d56be06a3082e
USER <USER@DESKTOP-9DC322O>
2021-01-02 22:06:54 +0200

    ךףגףצאי

--- dbo.Dish.data.sql
+++ dbo.Dish.data.sql
@@ -1,5 +1,6 @@
-﻿
-
+﻿--SET IDENTITY_INSERT [dbo].[Dish] ON
+--INSERT INTO [dbo].[Dish] ([Id], [Name], [Price], [Description], [Image]) VALUES (NULL, NULL, NULL, NULL, NULL)
+--SET IDENTITY_INSERT [dbo].[Dish] OFF
 SET IDENTITY_INSERT [dbo].[Dish] ON
 INSERT INTO [dbo].[Dish] ([Id], [Name], [Price], [Description], [Image]) VALUES (1, N'Sausage party', 47, N'.נקניקיות צ''וריסוס ביתיות, כרוב כבוש, קוביות צ''יפס, רוטב הולנדייז וביצת עין', N'https://files.mishloha.co.il/files/menu_food_pic/thumbnail/FIL_6119500_636458426331881339.jpg?v=2')
 INSERT INTO [dbo].[Dish] ([Id], [Name], [Price], [Description], [Image]) VALUES (2, N'Wild Goose', 61, N'.חזה אווז מעושן בפלאנצ''ה,צ''יפס בטטה,חסה,סלרי,תפוחים,שקדים,חמוציות בויניגרייט', N'https://files.mishloha.co.il/files/menu_food_pic/thumbnail/FIL_6069381_636227736520604721.jpg?v=2')
