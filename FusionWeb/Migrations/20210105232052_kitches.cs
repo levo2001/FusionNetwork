45b3001550f55d6336ed39d3e78005c68ea80677
USER <USER@DESKTOP-9DC322O>
2021-01-06 01:27:53 +0200

    wef

---
+++ FusionWeb/Migrations/20210105232052_kitches.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FusionWeb.Migrations
+{
+    public partial class kitches : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
